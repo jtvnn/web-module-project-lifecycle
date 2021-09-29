914bd4f5eafe6ad0bb62c9142b7350d4bd46f013
Jason Vann <vannjt1@icloud.com>
2021-09-29 00:57:00 -0400

    user card complete

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import User from './components/User'
+import Followers from './components/Followers'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>GitHub User-card</h1>
+    <User/>
     </div>
   );
 }
